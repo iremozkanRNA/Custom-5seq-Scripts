7cf6fd73073db43b596f91976d2196bb714f68fd
Irem Ozkan <32915625+iremozkanRNA@users.noreply.github.com>
2025-03-08 13:29:09 -0500

    Create Prep-Upstream-Sequences-for-MEME_bothAB.R

---
+++ Prep-Upstream-Sequences-for-MEME_bothAB.R
@@ -0,0 +1,44 @@
+rm(list = ls())
+lapply(c("dplyr", "tibble", "ggVennDiagram"), library ,character.only=T)
+
+setwd("~/Desktop/Stuff/AMeyer_Lab/Experiment_results/5seq/TSSPredIremOutput/")
+
+Data <- read.csv("ChlData_fRanked.csv", header = T)
+Data2 <- read.csv("KsgData_fRanked.csv", header = T)
+#######################################################
+
+################ TSS MOTIF Analysis #################
+
+#######################################################
+
+primary <- Data %>% filter(rank == "primary",  TSSUTR == "" & Locus != "" )
+primary$Genome[primary$Genome == "NDCt60"] <- "ChlNDCt60"
+primary2 <- Data2 %>% filter(rank == "primary",  TSSUTR == "" & Locus != "" )
+primary2$Genome[primary2$Genome == "NDCt60"] <- "KsgNDC60"
+primary <- rbind(primary, primary2)
+primary <- primary[!duplicated(primary$SuperPos),]
+fastaformat <- primary[,c(1,2,3,4,5,8,15)]
+fastaformat <- fastaformat %>% 
+  add_column(number = 1:nrow(fastaformat), .before = "SuperPos")
+fastaformat2 <- as.data.frame(fastaformat[,c(1)]) 
+fastaformat2[,c(2:8)] <- ""
+colnames(fastaformat2) <- colnames(fastaformat)
+fastaformat <- rbind(fastaformat, fastaformat2)
+fastaformat <- fastaformat %>% arrange(number)
+fastaformat$number <- paste( ">ID", fastaformat$number)
+fastaformat$number <- sub(" ", "", fastaformat$number)
+rownames(fastaformat) <- 1:nrow(fastaformat)
+
+for(i in 1:nrow(fastaformat)){
+  if(!fastaformat$SuperPos[i] == ""){
+    fastaformat$number[i] <- ""
+  }
+}
+
+fastaformat$Sequence..50.nt.upstream...TSS..51nt. <- paste(fastaformat$number, fastaformat$Sequence..50.nt.upstream...TSS..51nt.)
+fastaformat <- fastaformat[,-c(1)]
+rownames(fastaformat) <- 1:nrow(fastaformat)
+fastaformatPOS <- fastaformat
+fastaformatPOS <- fastaformat[-c(nrow(fastaformatPOS)),]
+
+write.csv(fastaformatPOS, file = "MEME_ChlKsg_primary_Genic.csv", sep = "", row.names = F, col.names = F)

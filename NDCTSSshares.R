85637c4976ad4108808297bfea649a5489214d62
Irem Ozkan <32915625+iremozkanRNA@users.noreply.github.com>
2025-06-30 16:16:23 -0400

    Update NDCTSSshares.R

--- NDCTSSshares.R
+++ NDCTSSshares.R
@@ -1,15 +1,16 @@
 rm(list=ls())
 lapply(c("dplyr", "UpSetR"), library, character.only=T)
 
-fh1 <- read.csv("~/Desktop/Stuff/AMeyer_Lab/Experiment_results/5seq/TSSPredIremOutput/ChlData_fRanked.csv") %>% filter(rank == "primary",Genome == "NDCt60")
-fh2 <- read.csv("~/Desktop/Stuff/AMeyer_Lab/Experiment_results/5seq/TSSPredIremOutput/KsgData_fRanked.csv") %>% filter(rank == "primary", Genome == "NDCt60")
+fh1 <- read.csv("~/Desktop/Stuff/AMeyer_Lab/Experiment_results/5seq/TSSPredIremOutput/ChlData_fRanked.csv") %>% filter(rank == "primary",Genome == "NDCt60", TSSUTR != "")
+fh2 <- read.csv("~/Desktop/Stuff/AMeyer_Lab/Experiment_results/5seq/TSSPredIremOutput/KsgData_fRanked.csv") %>% filter(rank == "primary", Genome == "NDCt60", TSSUTR != "")
 
-tss1 <- unique(fh1$Locus)
-tss2 <- unique((fh2$Locus))
+tss1 <- unique(fh1$SuperPos)
+tss2 <- unique((fh2$SuperPos))
 
 tss_list <- list(Chl = tss1, Ksg = tss2)
 
 upset( fromList(tss_list),
        sets = c("Chl", "Ksg"),
-       order.by="freq",
-       sets.bar.color = c("lightgreen", "pink"))
+       order.by="degree",
+       sets.bar.color = c("#108080", "#7F0440"))
+
